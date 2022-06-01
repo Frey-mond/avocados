a4f5cbbaaf4773fa9d0f945fbba131e1d219f561
ejk912 <ejk912@uw.edu>
2022-06-01 15:11:10 -0700

    Initial Framework for Task 3 + Cleaned f11ar.csv

--- task3.py
+++ task3.py
@@ -1,5 +1,24 @@
+"""
+CSE 163
+Eric Kim
+
+This file implements task 3 as outlined in the report.
+"""
 import pandas as pd
+import pickle
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
+def price_from_metric_model():
+    avocado_data = pd.read_csv('./data/avocado cleaned.csv', na_values=['---'])
+    metric_data = pd.read_csv('./data/f11ar cleaned.csv', na_values=['---'])
+    print(avocado_data.head())
+    print(metric_data.head())
+
+def main():
+    price_from_metric_model()
+
+
+if __name__ == '__main__':
+    main()
