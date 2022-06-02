ca3b9e228353a54a7122690e49e44ea1c90ea822
ejk912 <ejk912@uw.edu>
2022-06-01 17:15:28 -0700

    "Finished" with task 3

--- task3.py
+++ task3.py
@@ -7,14 +7,30 @@ This file implements task 3 as outlined in the report.
 import pandas as pd
 import pickle
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
 def price_from_metric_model():
+    # Formats data for model
     avocado_data = pd.read_csv('./data/avocado cleaned.csv', na_values=['---'])
     metric_data = pd.read_csv('./data/f11ar cleaned.csv', na_values=['---'])
-    print(avocado_data.head())
-    print(metric_data.head())
+    temp = avocado_data.groupby('year')['AveragePrice'].mean().round(2)
+    data = metric_data.merge(temp, on='year', how='left')
+    data = data.dropna()
+    # Assigns features and labels
+    features = data.loc[:, data.columns != 'AveragePrice']
+    features = pd.get_dummies(features)
+    labels = data['AveragePrice']
+    # Runs Model
+    features_train, features_test, labels_train, labels_test = \
+        train_test_split(features, labels, test_size=0.2, shuffle=False)
+    model = DecisionTreeRegressor()
+    model.fit(features_train, labels_train)
+    train_predictions = model.predict(features_train)
+    test_predictions = model.predict(features_test)
+    print('Training Mean Squared Error: ', mean_squared_error(labels_train, train_predictions))
+    print('Test Mean Squared Error', mean_squared_error(labels_test, test_predictions))
+
 
 def main():
     price_from_metric_model()
